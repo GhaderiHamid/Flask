e6515dd1d85819679b0506214adaed7252a5ae39
GhaderiHamid <161806332+GhaderiHamid@users.noreply.github.com>
2025-08-12 20:25:12 +0330

    Update main.py

--- main.py
+++ main.py
@@ -74,6 +74,8 @@ def recommend_products(user_id):
 
 #if __name__ == "__main__":
     #app.run(host="0.0.0.0", port=5000)
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
 @app.route("/recommend", methods=["GET"])
